1b71b9cc8c76b8217b7afc0d60b6097b13ffc7e2
Benjamin Nicol <benjamin.nicol17@gmail.com>
2024-06-14 16:25:02 +0200

    feat: adding tens handling on converter

--- FrenchNumberConverter/converter.py
+++ FrenchNumberConverter/converter.py
@@ -17,10 +17,13 @@ class Converter:
         "quatorze",
         "quinze",
         "seize",
+        "dix-sept",
+        "dix-huit",
+        "dix-neuf",
     ]
 
     TENS = [
-        "",
+        "zéro",
         "dix",
         "vingt",
         "trente",
@@ -42,9 +45,21 @@ class Converter:
         if number < 0:
             raise ValueError("Only positive integers are supported")
 
+        if number < 20:
+            return cls.UNITS[number]
+
         base_digit = number % 10
         base_ten_digit = number // 10
-        if base_ten_digit == 0:
-            return cls.UNITS[base_digit]
-        else:
-            return cls.TENS[base_ten_digit] + cls.UNITS[base_digit]
+
+        if base_ten_digit in {7, 9}:
+            base_digit += 10
+            base_ten_digit -= 1
+
+        if base_digit > 1:
+            return cls.TENS[base_ten_digit] + "-" + cls.UNITS[base_digit]
+
+        if base_digit == 1:
+            return cls.TENS[base_ten_digit] + "-et-" + cls.UNITS[base_digit]
+
+        if base_digit == 0:
+            return cls.TENS[base_ten_digit]
